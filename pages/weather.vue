31b983dee84a54ecb68aceebb5f68ea77a4491a0
Juan Manuel Béc <Juanmabs22@outlook.com>
2020-09-29 19:03:59 +0200

    Added tooltip to weather for example on input

--- pages/weather.vue
+++ pages/weather.vue
@@ -1,55 +1,37 @@
 <template>
   <section class="section">
-        <b-field>
-            <b-input placeholder="City, Country"
-                type="search"
-                icon="magnify"
-                v-model="newCity"
-            >
-            </b-input>
-            <p class="control">
-                <button class="button is-primary" @click="changeWeather()">See Weather</button>
-            </p>
-        </b-field>
-    <div class="columns is-desktop" v-if="weatherData.name">
-      
-      <card
-        title="City"
-        icon="city"
-      >
+    
+    <b-tooltip label="E.g. Madrid, ES">
+      <b-field>
+        <b-input placeholder="City, Country" type="search" icon="magnify" v-model="newCity">
+        </b-input>
+        <p class="control">
+          <button class="button is-primary" @click="changeWeather()">See Weather</button>
+        </p>
+      </b-field>
+    </b-tooltip>
+    <div class="columns is-desktop mt-1" v-if="weatherData.name">
+
+      <card title="City" icon="city">
         {{ weatherData.name }}
       </card>
-      <card
-        title="Country"
-        icon="island"
-      >
+      <card title="Country" icon="island">
         {{ weatherData.country }}
       </card>
-      <card
-        title="Location"
-        icon="crosshairs-gps"
-      >
-        {{ weatherData.coord.lon }},  {{ weatherData.coord.lat }}
+      <card title="Location" icon="crosshairs-gps">
+        {{ weatherData.coord.lon }}, {{ weatherData.coord.lat }}
       </card>
 
-      <card
-        :title="weatherData.weather.summary.title"
-        icon="brightness-5"
-      >
+      <card :title="weatherData.weather.summary.title" icon="brightness-5">
         {{ weatherData.weather.summary.description }}
       </card>
 
-      <card
-        title="Temperature"
-        icon="home-thermometer"
-      >
-        {{ (weatherData.weather.temperature.actual - 273.15).toFixed(1)  }} ºC ({{ (weatherData.weather.temperature.feelsLike - 273.15).toFixed(1) }} ºC)
+      <card title="Temperature" icon="home-thermometer">
+        {{ (weatherData.weather.temperature.actual - 273.15).toFixed(1) }} ºC
+        ({{ (weatherData.weather.temperature.feelsLike - 273.15).toFixed(1) }} ºC)
       </card>
 
-      <card
-        title="Wind"
-        icon="send"
-      >
+      <card title="Wind" icon="send">
         {{ weatherData.weather.wind.speed }} km/h
       </card>
     </div>
@@ -98,7 +80,6 @@ const query = gql`
     }
   }
 `
-
 export default {
   name: 'HomePage',
 
@@ -115,19 +96,19 @@ export default {
     this.getWeather();
   },
   watch: {
-    $route () {
+    $route() {
       this.getWeather();
     }
   },
   methods: {
-    async getWeather(newCity?: string) {
+    async getWeather(newCity ? : string) {
       try {
-          const data = await request('https://graphql-weather-api.herokuapp.com/', query, {
-            city: newCity || this.$route.query["city"] || "Madrid, ES"
-          })
-          this.weatherData = data.getCityByName;
+        const data = await request('https://graphql-weather-api.herokuapp.com/', query, {
+          city: newCity || this.$route.query["city"] || "Madrid, ES"
+        })
+        this.weatherData = data.getCityByName;
       } catch (e) {
-          console.error(e)
+        console.error(e)
       }
     },
     changeWeather() {
