588f85f3382a2fb97081c35a728abdd859b85d4d
Juan Manuel Béc <Juanmabs22@outlook.com>
2020-09-29 16:56:24 +0200

    Added search to table parkings

--- pages/parkings.vue
+++ pages/parkings.vue
@@ -22,22 +22,6 @@
             <div class="control is-flex">
                 <b-switch v-model="isPaginationSimple" :disabled="!isPaginated">Simple pagination</b-switch>
             </div>
-            <!-- <b-select v-model="paginationPosition" :disabled="!isPaginated">
-                <option value="bottom">bottom pagination</option>
-                <option value="top">top pagination</option>
-                <option value="both">both</option>
-            </b-select>
-            <b-select v-model="sortIcon">
-                <option value="arrow-up">Arrow sort icon</option>
-                <option value="menu-up">Caret sort icon</option>
-                <option value="chevron-up">Chevron sort icon </option>
-            </b-select>
-            <b-select v-model="sortIconSize">
-                <option value="is-small">Small sort icon</option>
-                <option value="">Regular sort icon</option>
-                <option value="is-medium">Medium sort icon</option>
-                <option value="is-large">Large sort icon</option>
-            </b-select> -->
         </b-field>
 
         <b-table
@@ -57,16 +41,13 @@
             aria-page-label="Page"
             aria-current-label="Current page">
 
-            <b-table-column field="id" label="ID" sortable numeric v-slot="props">
+            <b-table-column field="id" label="ID" sortable numeric v-slot="props" searchable>
                 <a :href="props.row['@id']"> {{ props.row.id }} </a>
             </b-table-column>
-            <b-table-column field="type" label="Type" sortable numeric v-slot="props">
-                <a :href="props.row['@type']"> @type </a>
-            </b-table-column>
-            <b-table-column field="name" label="Name" sortable numeric v-slot="props">
+            <b-table-column field="title" label="Name" sortable v-slot="props" searchable>
                 {{ props.row.title }}
             </b-table-column>
-            <b-table-column field="address" label="Address" sortable numeric v-slot="props">
+            <b-table-column field="fullAddress" label="Address" sortable v-slot="props" searchable>
                 <span v-html="props.row.address['street-address']"></span>
                 <br>
                 <span v-html="props.row.address.districtName"></span>
@@ -84,7 +65,7 @@
               {{ props.row.distance.toFixed(1) }} km
             </b-table-column>
             
-            <b-table-column field="maps" label="Maps" sortable numeric v-slot="props">
+            <b-table-column field="maps" label="Maps" v-slot="props">
               <a
                 :href="`http://www.google.com/maps/place/${ props.row.location.latitude },${ props.row.location.longitude }`"
                 target="_blank"
@@ -157,6 +138,12 @@ export default {
             for (let parking of this.data) {
               parking.address.districtName = parking.address.district["@id"].substring(parking.address.district["@id"].lastIndexOf("/") + 1);
               parking.address.areaName = parking.address.area["@id"].substring(parking.address.area["@id"].lastIndexOf("/") + 1);
+              parking.fullAddress = 
+                unescape(parking.address['street-address']) + 
+                unescape(parking.address.districtName) + 
+                unescape(parking.address.areaName) + 
+                unescape(parking.address['postal-code']) + 
+                unescape(parking.address.locality);
             }
           },
           async tryGetDistance() {
